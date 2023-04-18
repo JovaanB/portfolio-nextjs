1e3a1182fe0ee27dadad1664a85a21cb52179db7
Jovan <jovan@teamr.eu>
2023-04-18 21:32:55 +0200

    Update meta og

--- content/meta.js
+++ content/meta.js
@@ -3,57 +3,57 @@ export default {
     title: "",
     description:
       "Hey, I am Jovan Bienvenu. A Front-end Developer/React Developer from France who loves to code. I use React.js or Next.js to build the web application interfaces and the functionalities. At the moment, I am pursuing my Bachelor's degree in Computer Science.",
-    image: "https://imgur.com/KeJgIVl.png",
-    keywords: "portfolio jatin, portfolio j471n, jatin blogs",
+    image: "https://media.licdn.com/dms/image/C5603AQHtmTmqZGSwog/profile-displayphoto-shrink_400_400/0/1646292031599?e=1686182400&v=beta&t=q_zDdBVwb7iySN8jdycJATMotQS5rwndDGTf8TOhY-0",
+    keywords: "portfolio, blog, jovan bienvenu",
   },
 
   stats: {
     title: "Statistics -",
     description:
       "These are my personal statistics about me. It includes My Blogs and github Stats and top music stats.",
-    image: "https://imgur.com/9scFfW5.png",
+    image: "https://media.licdn.com/dms/image/C5603AQHtmTmqZGSwog/profile-displayphoto-shrink_400_400/0/1646292031599?e=1686182400&v=beta&t=q_zDdBVwb7iySN8jdycJATMotQS5rwndDGTf8TOhY-0",
     keywords: "stats, Statistics",
   },
   utilities: {
     title: "Utilities - ",
     description:
       "In case you are wondering What tech I use, Here's the list of what tech I'm currently using for coding on the daily basis. This list is always changing.",
-    image: "https://imgur.com/MpfymCd.png",
+    image: "https://media.licdn.com/dms/image/C5603AQHtmTmqZGSwog/profile-displayphoto-shrink_400_400/0/1646292031599?e=1686182400&v=beta&t=q_zDdBVwb7iySN8jdycJATMotQS5rwndDGTf8TOhY-0",
     keywords: "Utilities, what i use?, utils, setup, uses,",
   },
   blogs: {
     title: "Blogs -",
     description:
-      "I've been writing online since 2022, mostly about web development and tech careers. In total, I've written more than 50 articles till now.",
-    image: "https://imgur.com/nbNLLZk.png",
-    keywords: "j471n blog, blog, webdev, react",
+      "I've been writing online since 2022, mostly about web development.",
+    image: "https://media.licdn.com/dms/image/C5603AQHtmTmqZGSwog/profile-displayphoto-shrink_400_400/0/1646292031599?e=1686182400&v=beta&t=q_zDdBVwb7iySN8jdycJATMotQS5rwndDGTf8TOhY-0",
+    keywords: "jovan bienvenu blog, blog, webdev, react",
   },
 
   bookmark: {
     title: "Bookmarks -",
     description: "Bookmarked Blogs of Jovan Bienvenu's blogs by you",
-    image: "https://imgur.com/5XkrVPq.png",
+    image: "https://media.licdn.com/dms/image/C5603AQHtmTmqZGSwog/profile-displayphoto-shrink_400_400/0/1646292031599?e=1686182400&v=beta&t=q_zDdBVwb7iySN8jdycJATMotQS5rwndDGTf8TOhY-0",
     keywords: "bookmark, blogs, ",
   },
   certificates: {
     title: "Certificates -",
     description:
       "I've participated in many contests, courses and test and get certified in many skills. You can find the certificates below.",
-    image: "https://imgur.com/J0q1OdT.png",
+    image: "https://media.licdn.com/dms/image/C5603AQHtmTmqZGSwog/profile-displayphoto-shrink_400_400/0/1646292031599?e=1686182400&v=beta&t=q_zDdBVwb7iySN8jdycJATMotQS5rwndDGTf8TOhY-0",
     keywords: "Certificates, verified",
   },
   projects: {
     title: "Projects -",
     description:
       "I've been making various types of projects some of them were basics and some of them were complicated.",
-    image: "https://imgur.com/XJqiuNK.png",
+    image: "https://media.licdn.com/dms/image/C5603AQHtmTmqZGSwog/profile-displayphoto-shrink_400_400/0/1646292031599?e=1686182400&v=beta&t=q_zDdBVwb7iySN8jdycJATMotQS5rwndDGTf8TOhY-0",
     keywords: "projects, work, side project,",
   },
   about: {
     title: "About -",
     description:
-      "Hey, I am Jovan Bienvenu. A Front-end Developer/React Developer from India who loves to design and code. I use React.js or Next.js to build the web application interfaces and the functionalities. At the moment, I am pursuing my Bachelor's degree in Computer Science.",
-    image: "https://imgur.com/b0HRaPv.png",
+      "Hey, I am Jovan Bienvenu. A Front-end Developer/React Developer from France who loves to design and code. I use React.js or Next.js to build the web application interfaces and the functionalities. At the moment, I am pursuing my Bachelor's degree in Computer Science.",
+    image: "https://media.licdn.com/dms/image/C5603AQHtmTmqZGSwog/profile-displayphoto-shrink_400_400/0/1646292031599?e=1686182400&v=beta&t=q_zDdBVwb7iySN8jdycJATMotQS5rwndDGTf8TOhY-0",
     keywords: "about",
   },
 };
